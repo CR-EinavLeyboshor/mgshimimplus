b5f0ebbd80250ff5e13a5209c7e0227bbd93f27c
cr-prod-hp-1-k8s-deploymentbox <einav.leyboshor@cybereason.com>
2023-09-22 23:41:28 +0300

    add groovy pipeline

---
+++ jenkins/deploy-app.groovy
@@ -0,0 +1,42 @@
+// ed08f3a9e0d646b9b0f6adddb5cb6429
+final String url = "34.85.236.100"
+pipeline {
+    agent { docker { image 'docker:latest' } }
+    stages {
+        stage('build') {
+            steps {
+                sh 'docker build -t  cybereason/magshimim-web:0.0.1 web-server'
+            }
+        }
+         stage('push') {
+            steps {
+                withCredentials([string(credentialsId: 'DockerHubPwd', variable: 'dockerpwd')]) {
+                sh """
+                    docker login -u recyber -p ${dockerpwd} 
+                    docker push  cybereason/magshimim-web:0.0.1
+                """
+                }
+
+            }
+    }
+    stage('redeploy'){
+        steps{
+            sshagent(credentials : ['ssh-einav-inst']) {
+            sh "ssh -o StrictHostKeyChecking=no einav.leybsohor@${url} sudo docker stop magshimim | true"
+            sh "ssh -o StrictHostKeyChecking=no einav.leybsohor@${url} sudo docker run --pull=always --name=magshimim --rm -d -p 80:80  cybereason/magshimim-web:0.0.1"
+    
+        }
+        }
+    }
+
+    stage('Tests'){
+         agent { docker { image 'curlimages/curl' } }
+        steps{
+            script {
+                    final String response = sh(script: "curl -s $url", returnStdout: true).trim()
+                    echo response
+                }
+        }
+    }
+    }
+}
